b6eb8254a6e421b125cb6026e43dc88859fe304b
Riley S <substitute@pocketdevs.org>
2022-12-08 02:04:12 -0500

    Initial v3 release
    Still a few things to clean up.
    
    Need to implement `field_ptr` !

--- hookableFunction.h
+++ hookableFunction.h
@@ -2,47 +2,42 @@
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 #include <concepts>
-#include <string>
+#include <functional>
+#include <type_traits>
 
 #pragma region Definitions
-    #if defined(__GNUC__) || defined(__clang__)
-    #define CC_CDECL __attribute__((cdecl))
-    #define CC_STDCALL __attribute__((stdcall))
-    #define CC_FASTCALL __attribute__((fastcall))
-    #define CC_VECTORCALL __attribute__((vectorcall))
-    #if defined(__INTEL_COMPILER)
-    #define CC_REGCALL __attribute__((regcall))
-    #else
-    #define CC_REGCALL CC_CDECL
-    #endif
-    #elif defined(_MSC_VER) || defined(__INTEL_COMPILER)
-    #define CC_CDECL __cdecl
-    #define CC_STDCALL __stdcall
-    #define CC_THISCALL __thiscall
-    #define CC_FASTCALL __fastcall
-    #define CC_VECTORCALL __vectorcall
-    #if defined(__INTEL_COMPILER)
-    #define CC_REGCALL __regcall
-    #else
-    #define CC_REGCALL CC_CDECL
-    #endif
-    #else
-    #define CC_CDECL
-    #define CC_STDCALL
-    #define CC_FASTCALL
-    #define CC_VECTORCALL
-    #define CC_REGCALL
-    #endif
+#if defined(__GNUC__) || defined(__clang__)
+#define CC_CDECL __attribute__((cdecl))
+#define CC_STDCALL __attribute__((stdcall))
+#define CC_FASTCALL __attribute__((fastcall))
+#define CC_VECTORCALL __attribute__((vectorcall))
+#if defined(__INTEL_COMPILER)
+#define CC_REGCALL __attribute__((regcall))
+#else
+#define CC_REGCALL CC_CDECL
+#endif
+#elif defined(_MSC_VER) || defined(__INTEL_COMPILER)
+#define CC_CDECL __cdecl
+#define CC_STDCALL __stdcall
+#define CC_THISCALL __thiscall
+#define CC_FASTCALL __fastcall
+#define CC_VECTORCALL __vectorcall
+#if defined(__INTEL_COMPILER)
+#define CC_REGCALL __regcall
+#else
+#define CC_REGCALL CC_CDECL
+#endif
+#else
+#define CC_CDECL
+#define CC_STDCALL
+#define CC_FASTCALL
+#define CC_VECTORCALL
+#define CC_REGCALL
+#endif
 #pragma endregion
-/*
-Thanks to Raymond Chen (oldnewthing) for the above macros-to-compiler specific attributes.
-		https://github.com/oldnewthing/
-https://devblogs.microsoft.com/oldnewthing/20200716-00/?p=103986
-*/
 
-#pragma region Dummy Structures
-struct VA_ARGS {};
-struct NO_VA {};
+#pragma region Helpers
+struct variadic_t {};
 
 struct calling_convention {};
 struct cdecl_t : calling_convention {};
@@ -52,182 +47,126 @@ struct fastcall_t : calling_convention {};
 struct vectorcall_t : calling_convention {};
 struct regcall_t : calling_convention {};
 
-#pragma endregion
-
-#pragma region Concepts
 template<typename T>
-concept is_valid_va = std::is_same_v<T, NO_VA> || std::is_same_v<T, VA_ARGS>; //checks if the type is a VA or NO_VA type.
+concept Function = std::is_function_v<T>;
 
 template<typename T>
-concept is_valid_cc = std::derived_from<T, calling_convention>; //checks if the type is a valid calling convention type.
+concept CallingConvention = std::derived_from<T, calling_convention>;
 
 template<typename T>
-concept is_va = std::is_same_v<VA_ARGS, T>; //checks if the type is VA.
+concept Variadic = std::is_same_v<T, variadic_t> || std::is_same_v<T, void>;
 
-template<typename F, typename CC, typename VA>
-concept is_valid_function = std::is_function_v<F> && is_valid_va<VA> && is_valid_cc<CC>; //composite check for if all types are valid.
-#pragma endregion
-
-
-#pragma region Calling Convention Pointers
-template<typename RT, typename CC, typename ...A> requires is_valid_function<RT(A...), CC, NO_VA> //base case NO_VA
+template<Function T1, CallingConvention T2, Variadic T3>
 struct signature { };
 
-template<typename RT, typename CC, typename ...A> requires is_valid_function<RT(A...), CC, VA_ARGS> //base case VA
-struct signature_va { };
-
-#pragma region NO VA Specializations
 template<typename RT, typename ...A>
-struct signature<RT, cdecl_t, A...> { using type = RT(CC_CDECL*)(A...); };
+struct signature<RT(A...), cdecl_t, void> { using type = RT(CC_CDECL)(A...); };
 
 template<typename RT, typename ...A>
-struct signature<RT, stdcall_t, A...> { using type = RT(CC_STDCALL*)(A...); };
+struct signature<RT(A...), cdecl_t, variadic_t> { using type = RT(CC_CDECL)(A..., ...); };
 
 template<typename RT, typename ...A>
-struct signature<RT, thiscall_t, A...> { using type = RT(CC_THISCALL*)(A...); };
+struct signature<RT(A...), stdcall_t, void> { using type = RT(CC_STDCALL)(A...); };
 
 template<typename RT, typename ...A>
-struct signature<RT, fastcall_t, A...> { using type = RT(CC_FASTCALL*)(A...); };
+struct signature<RT(A...), stdcall_t, variadic_t> { using type = RT(CC_STDCALL)(A..., ...); };
 
 template<typename RT, typename ...A>
-struct signature<RT, vectorcall_t, A...> { using type = RT(CC_VECTORCALL*)(A...); };
+struct signature<RT(A...), thiscall_t, void> { using type = RT(CC_THISCALL)(A...); };
+
+/* thiscall can't have ... on x86, thiscall collapses to fastcall on x64(?), left here for compatability, probably should warn on use. */
+template<typename RT, typename ...A>
+struct signature<RT(A...), thiscall_t, variadic_t> { using type = RT(CC_FASTCALL)(A..., ...); };
+/* ----------------------------------------------------------------------------------------------------------------------------------- */
+template<typename RT, typename ...A>
+struct signature<RT(A...), fastcall_t, void> { using type = RT(CC_FASTCALL)(A...); };
 
 template<typename RT, typename ...A>
-struct signature<RT, regcall_t, A...> { using type = RT(CC_REGCALL*)(A...); };
+struct signature<RT(A...), fastcall_t, variadic_t> { using type = RT(CC_FASTCALL)(A..., ...); };
+
+template<typename RT, typename ...A>
+struct signature<RT(A...), vectorcall_t, void> { using type = RT(CC_VECTORCALL)(A...); };
+
+template<typename RT, typename ...A>
+struct signature<RT(A...), vectorcall_t, variadic_t> { using type = RT(CC_VECTORCALL)(A..., ...); };
+
+template<typename RT, typename ...A>
+struct signature<RT(A...), regcall_t, void> { using type = RT(CC_REGCALL)(A...); };
+
+template<typename RT, typename ...A>
+struct signature<RT(A...), regcall_t, variadic_t> { using type = RT(CC_REGCALL)(A...,...); };
+
 #pragma endregion
 
-#pragma region VA Specializations
-template<typename RT, typename ...A>
-struct signature_va<RT, cdecl_t, A...> { using type = RT(CC_CDECL*)(A..., ...); };
-
-template<typename RT, typename ...A>
-struct signature_va<RT, stdcall_t, A...> { using type = RT(CC_STDCALL*)(A..., ...); };
-
-template<typename RT, typename ...A>
-struct signature_va<RT, fastcall_t, A...> { using type = RT(CC_FASTCALL*)(A..., ...); };
-
-template<typename RT, typename ...A>
-struct signature_va<RT, vectorcall_t, A...> { using type = RT(CC_VECTORCALL*)(A..., ...); };
-
-template<typename RT, typename ...A>
-struct signature_va<RT, regcall_t, A...> { using type = RT(CC_REGCALL*)(A..., ...); };
-#pragma endregion
-#pragma endregion
-
-
-class IHookable {};
-
-template<typename RT, typename CC, typename VA> requires is_valid_function<RT, CC, VA>
-class hookableFunction : public IHookable {}; //Empty default template
-
-template<typename RT, typename CC, typename VA, typename ...A>
-class hookableFunction<RT(A...), CC, VA>
+template<typename T>
+class extern_ptr
 {
-    void* immutable_ptr = 0;
-    void* address_ptr = 0;
-
-    static inline auto from_virtual(void* object, int index) -> void*
-    {
-        return (*static_cast<void***>(object))[index];
-    }
-
-    static inline auto from_module(LPCTSTR module, int offset) -> void*
-    {
-        return reinterpret_cast<void*>(reinterpret_cast<int>(GetModuleHandle(module)) + offset);
-    }
-    using signature_t = std::conditional_t<is_va<VA>, typename signature_va<RT, CC, A...>::type, typename signature<RT, CC, A...>::type>; //create the function pointer w/ calling convention.
+private:
+protected:
+	struct type_identity { using type = T; };
+	type_identity::type* immutable_ptr;
 public:
-	explicit hookableFunction(void* address)
-    {
-        address_ptr = address;
-        immutable_ptr = address;
-    }
-    
-    hookableFunction(LPCTSTR module, int offset) : hookableFunction(from_module(module, offset)) {} /* module + offset */
-    
-    hookableFunction(void* object, int index) : hookableFunction(from_virtual(object, index)) {} /* object + from virtual function index */
 
-    hookableFunction() : hookableFunction(nullptr) {} /* No Arg Constructor */
+	[[nodiscard]] auto get() { return immutable_ptr; }
 
-	[[nodiscard]] auto immutable() const -> void*
-    {
-        return immutable_ptr; /* Read-Only copy of the original pointer value */
-    }
+	[[nodiscard]] auto operator->() { return immutable_ptr; }
 
-	[[nodiscard]] auto address() const -> void*
-    {
-        return address_ptr; /* Read-Only copy of the address */
-    }
+	[[nodiscard]] auto operator*() { return *immutable_ptr; } /* when T is function* T is disallowed, auto returns T* instead of T for* T for functions. */
+	void reset(type_identity::type* new_ptr) { this->immutable_ptr = new_ptr; }
 
-    [[nodiscard]] auto get() -> void**
-    {
-        return &address_ptr;
-    }
-
-    auto reset(void* address)
-    {
-        address_ptr = address;
-        immutable_ptr = address;
-    }
-    auto reset() -> void { reset(nullptr); }
-    auto reset(LPCTSTR module, int offset) { reset(from_module(module, offset)); }
-    auto reset(void* object, int index) { reset(from_virtual(object, index)); }
-
-    /// <summary>
-    /// Check if function we pointed too has been modified (e.g. Hooked.)
-    /// </summary>
-    /// <returns>True if the function has been modifed.</returns>
-    auto dirty() -> bool { return immutable_ptr != address_ptr; }
-
-    /// <summary>
-    /// This calls the original address. This means it will include your hook if you have set one.
-    /// </summary>
-    /// <param name="...args">The args to pass into the function.</param>
-    /// <returns>The type specified in the hook.</returns>
-    RT operator()(A... args) requires !is_va<VA> {
-        if (!immutable_ptr) throw "Attempted to call null pointer.";
-        return static_cast<signature_t>(immutable_ptr)(args...);
-    }
-
-    /// <summary>
-    /// This calls the original address. This means it will include your hook if you have set one.
-    /// </summary>
-    /// <param name="...args">The args to pass into the function.</param>
-    /// <param name="...va">The VA_ARGS to pass into the function.</param>
-    /// <returns>The type specified in the hook.</returns>
-    template<typename ...B> requires is_va<VA>
-    RT operator()(A... args, B... va) {
-        if (!immutable_ptr) throw std::exception("Attempted to call null pointer.");
-        return static_cast<signature_t>(immutable_ptr)(args..., va...);
-    }
-
-    /// <summary>
-    /// This calls the original function by address. This is what you want to call from your hook.
-    /// </summary>
-    /// <param name="...args">The args to pass into the function.</param>
-    /// <returns>The type specified in the hook.</returns>
-    RT original(A... args) requires !is_va<VA>
-    {
-        if (!address_ptr) throw std::exception("Attempted to call null pointer.");
-        return static_cast<signature_t>(address_ptr)(args...);
-    }
-
-    /// <summary>
-    /// This calls the original function by address. This is what you want to call from your hook.
-    /// </summary>
-    /// <param name="...args">The args to pass into the function.</param>
-    /// <param name="...va">The VA_ARGS to pass into the function.</param>
-    /// <returns>The type specified in the hook.</returns>
-    template<typename ...B> requires is_va<VA>
-    RT original(A... args, B... va)
-    {
-        if (!address_ptr) throw "Attempted to call null pointer.";
-        return static_cast<signature_t>(address_ptr)(args..., va...);
-    }
+	explicit extern_ptr(void* address)
+	{
+		immutable_ptr = reinterpret_cast<type_identity::type*>(address);
+	}
+	extern_ptr() : extern_ptr(nullptr) {};
 };
 
-template<typename T, typename U = cdecl_t>
-using hookable  = hookableFunction<T, U, NO_VA>;
+template<Function T1, CallingConvention T2 = cdecl_t, Variadic T3 = void>
+class function_pointer {};
 
-template<typename T, typename U = cdecl_t>
-using hookable_va = hookableFunction<T, U, VA_ARGS>;
+template<typename RT, CallingConvention T2, Variadic T3, typename ...A>
+class function_pointer<RT(A...), T2, T3> : public extern_ptr<typename signature<RT(A...), T2, T3>::type> /* typename not declared yet. */
+{
+	using base = extern_ptr<typename signature<RT(A...), T2, T3>::type>;
+
+	base::type_identity::type* mutable_ptr = 0;
+
+	static inline auto from_module(LPCTSTR module, int offset) -> void*
+	{
+		return reinterpret_cast<void*>(reinterpret_cast<int>(GetModuleHandle(module)) + offset);
+	}
+
+public:
+	explicit function_pointer(void* address) : base(address)
+	{
+		mutable_ptr =  reinterpret_cast<base::type_identity::type*>(address);
+	}
+	function_pointer(LPCTSTR module, int offset) : function_pointer(from_module(module, offset)) {} /* module + offset */
+	
+	function_pointer() : function_pointer(nullptr) {};
+
+	[[nodiscard]] auto mut() { return (void**) &mutable_ptr; } /* pointer to the mutable pointer, decayed of all type info. */
+
+	RT operator()(A... args) requires std::is_same_v<void, T3>
+	{
+		return base::immutable_ptr(args...);
+	}
+
+	template<typename ...B>
+	RT operator()(A... args, B... vargs) requires std::is_same_v<variadic_t, T3>
+	{
+		return base::immutable_ptr(args..., vargs...);
+	}
+
+	RT original(A... args) requires std::is_same_v<void, T3>
+	{
+		return mutable_ptr(args...);
+	}
+
+	template<typename ...B>
+	RT original(A... args, B... vargs) requires std::is_same_v<variadic_t, T3>
+	{
+		return mutable_ptr(args..., vargs...);
+	}
+
+};
