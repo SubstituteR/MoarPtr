7f68f7c864f068773f90b0414df2768e92d45384
Riley S <substitute@pocketdevs.org>
2022-12-16 17:27:01 -0500

    More compilers support.
    - Work around Clang syntax issues
    - Isolate Windows Specific Code with Macros

--- function_pointer.h
+++ function_pointer.h
@@ -3,14 +3,17 @@
 #include <functional>
 #include "moar_ptr.h"
 
+#ifdef _WIN32
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
+#endif
 
 #pragma region Definitions
 
 #if defined(__GNUC__) || defined(__clang__)
 #define CC_CDECL __attribute__((cdecl))
 #define CC_STDCALL __attribute__((stdcall))
+#define CC_THISCALL __attribute__((thiscall))
 #define CC_FASTCALL __attribute__((fastcall))
 #define CC_VECTORCALL __attribute__((vectorcall))
 #if defined(__INTEL_COMPILER)
@@ -32,9 +35,17 @@
 #else
 #define CC_CDECL
 #define CC_STDCALL
+#define CC_THISCALL
 #define CC_FASTCALL
 #define CC_VECTORCALL
 #define CC_REGCALL
+#endif
+#if defined(__clang__) /* Clang does not support B(A)(C...) syntax, only A B(C...) and refuses to compile otherwise. */
+#define FUNCTION_SIGNATURE(x,y,z) y x(z)
+#define FUNCTION_SIGNATURE_VA(x,y,z) y x(z, ...)
+#elif defined(__GNUC__) || defined(_MSC_VER) || defined(__INTEL_COMPILER) /* GCC Supports A B(C...)  and B(A)(C...) but produces wrong T* for T under A B(C...) */
+#define FUNCTION_SIGNATURE(x,y,z) y(x)(z)
+#define FUNCTION_SIGNATURE_VA(x,y,z) y(x)(z,...)
 #endif
 
 #pragma endregion
@@ -64,41 +75,41 @@ template<Function T1, CallingConvention T2, Variadic T3>
 struct signature { };
 
 template<typename RT, typename ...A>
-struct signature<RT(A...), cdecl_t, void> { using type = RT(CC_CDECL)(A...); };
+struct signature<RT(A...), cdecl_t, void> { using type = FUNCTION_SIGNATURE(CC_CDECL, RT, A...); };
 
 template<typename RT, typename ...A>
-struct signature<RT(A...), cdecl_t, variadic_t> { using type = RT(CC_CDECL)(A..., ...); };
+struct signature<RT(A...), cdecl_t, variadic_t> { using type = FUNCTION_SIGNATURE_VA(CC_CDECL, RT, A...);};
 
 template<typename RT, typename ...A>
-struct signature<RT(A...), stdcall_t, void> { using type = RT(CC_STDCALL)(A...); };
+struct signature<RT(A...), stdcall_t, void> { using type = FUNCTION_SIGNATURE(CC_STDCALL, RT, A...);};
 
 template<typename RT, typename ...A>
-struct signature<RT(A...), stdcall_t, variadic_t> { using type = RT(CC_STDCALL)(A..., ...); };
+struct signature<RT(A...), stdcall_t, variadic_t> { using type = FUNCTION_SIGNATURE_VA(CC_STDCALL, RT, A...);};
 
 template<typename RT, typename ...A>
-struct signature<RT(A...), thiscall_t, void> { using type = RT(CC_THISCALL)(A...); };
+struct signature<RT(A...), thiscall_t, void> { using type = FUNCTION_SIGNATURE(CC_THISCALL, RT, A...);};
 
 /* thiscall can't have ... on x86, thiscall collapses to fastcall on x64(?), left here for compatability, probably should warn on use. */
 template<typename RT, typename ...A>
-struct signature<RT(A...), thiscall_t, variadic_t> { using type = RT(CC_FASTCALL)(A..., ...); };
+struct signature<RT(A...), thiscall_t, variadic_t> { using type = FUNCTION_SIGNATURE_VA(CC_FASTCALL, RT, A...);};
 /* ----------------------------------------------------------------------------------------------------------------------------------- */
 template<typename RT, typename ...A>
-struct signature<RT(A...), fastcall_t, void> { using type = RT(CC_FASTCALL)(A...); };
+struct signature<RT(A...), fastcall_t, void> { using type = FUNCTION_SIGNATURE(CC_FASTCALL, RT, A...);};
 
 template<typename RT, typename ...A>
-struct signature<RT(A...), fastcall_t, variadic_t> { using type = RT(CC_FASTCALL)(A..., ...); };
+struct signature<RT(A...), fastcall_t, variadic_t> { using type = FUNCTION_SIGNATURE_VA(CC_FASTCALL, RT, A...); };
 
 template<typename RT, typename ...A>
-struct signature<RT(A...), vectorcall_t, void> { using type = RT(CC_VECTORCALL)(A...); };
+struct signature<RT(A...), vectorcall_t, void> { using type = FUNCTION_SIGNATURE(CC_VECTORCALL, RT, A...);};
 
 template<typename RT, typename ...A>
-struct signature<RT(A...), vectorcall_t, variadic_t> { using type = RT(CC_VECTORCALL)(A..., ...); };
+struct signature<RT(A...), vectorcall_t, variadic_t> { using type = FUNCTION_SIGNATURE_VA(CC_VECTORCALL, RT, A...); };
 
 template<typename RT, typename ...A>
-struct signature<RT(A...), regcall_t, void> { using type = RT(CC_REGCALL)(A...); };
+struct signature<RT(A...), regcall_t, void> { using type = FUNCTION_SIGNATURE(CC_REGCALL, RT, A...);};
 
 template<typename RT, typename ...A>
-struct signature<RT(A...), regcall_t, variadic_t> { using type = RT(CC_REGCALL)(A..., ...); };
+struct signature<RT(A...), regcall_t, variadic_t> { using type = FUNCTION_SIGNATURE_VA(CC_REGCALL, RT, A...); };
 
 #pragma endregion
 
@@ -112,10 +123,12 @@ class function_pointer<RT(A...), T2, T3> : public extern_ptr<typename signature<
 
 	base::type_identity::type* mutable_ptr = 0;
 
+#ifdef _WIN32
 	static inline auto from_module(LPCTSTR module, int offset) -> void*
 	{
 		return reinterpret_cast<void*>(reinterpret_cast<int>(GetModuleHandle(module)) + offset);
 	}
+#endif
 
 	void reset_internal(base::type_identity::type* new_ptr) override { this->mutable_ptr = new_ptr; }
 
@@ -124,7 +137,10 @@ public:
 	{
 		mutable_ptr = reinterpret_cast<base::type_identity::type*>(address);
 	}
+
+#ifdef _WIN32
 	function_pointer(LPCTSTR module, int offset) : function_pointer(from_module(module, offset)) {} /* module + offset */
+#endif
 
 	function_pointer() : function_pointer(nullptr) {};
 
@@ -154,3 +170,12 @@ public:
 	}
 
 };
+
+#undef CC_CDECL
+#undef CC_STDCALL
+#undef CC_THISCALL
+#undef CC_FASTCALL
+#undef CC_VECTORCALL
+#undef CC_REGCALL
+#undef FUNCTION_SIGNATURE
+#undef FUNCTION_SIGNATURE_VA
