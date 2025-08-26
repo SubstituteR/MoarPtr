4ae1f572e7bc0cd0c1ca79a3d62e503a84e7506a
Riley S <substitute@pocketdevs.org>
2025-08-26 19:05:59 -0400

    Add sysv and ms_abi to calling conventions

--- moar_ptr.h
+++ moar_ptr.h
@@ -8,6 +8,8 @@
 #define CC_THISCALL __attribute__((thiscall))
 #define CC_FASTCALL __attribute__((fastcall))
 #define CC_VECTORCALL __attribute__((vectorcall))
+#define CC_MS_ABI __attribute__((ms_abi))
+#define CC_SYSV_ABI __attribute__((sysv_abi))
 #if defined(__INTEL_COMPILER)
 #define CC_REGCALL __attribute__((regcall))
 #else
@@ -19,6 +21,8 @@
 #define CC_THISCALL __fastcall // temporary compiler fix for https://github.com/SubstituteR/MoarPtr/issues/4
 #define CC_FASTCALL __fastcall
 #define CC_VECTORCALL __vectorcall
+#define CC_MS_ABI CC_CDECL
+#define CC_SYSV_ABI CC_CDECL
 #if defined(__INTEL_COMPILER)
 #define CC_REGCALL __regcall
 #else
@@ -31,6 +35,8 @@
 #define CC_FASTCALL
 #define CC_VECTORCALL
 #define CC_REGCALL
+#define CC_MS_ABI
+#define CC_SYSV_ABI
 #endif
 
 #if defined(__clang__)
@@ -70,7 +76,7 @@
 #undef CC_FASTCALL
 #undef CC_VECTORCALL
 #undef CC_REGCALL
-#undef FUNCTION_SIGNATURE
-#undef FUNCTION_SIGNATURE_VA
+#undef CC_MS_ABI
+#undef CC_SYSV_ABI
 
 #pragma endregion
