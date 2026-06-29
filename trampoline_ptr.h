3940e7de0616ea8e6679d3df10fc4ddeb2083997
Riley S. <Github@rileystrickland.com>
2026-06-29 01:36:40 -0400

    Combine macro definition

--- trampoline_ptr.h
+++ trampoline_ptr.h
@@ -10,11 +10,11 @@ namespace moar
     class trampoline_ptr;
 
 
-#define trampoline_ptr_impl(convention) \
-    template<typename RT, typename ...A> requires moar::is_calling_convention_active_v<RT(A...), types::convention##_t, void> \
-    class trampoline_ptr<RT convention##_m(A...)> final : public trampoline_ptr_base<RT(A...), types::convention##_t, void> \
+#define trampoline_ptr_impl(convention, ...) \
+    template<typename RT, typename ...A> requires moar::is_calling_convention_active_v<RT(A...), types::convention##_t, std::conditional_t<false __VA_OPT__( | true), types::variadic_t, void>> \
+    class trampoline_ptr<RT convention##_m(A... __VA_OPT__(,__VA_ARGS__))> final : public trampoline_ptr_base<RT(A...), types::convention##_t, std::conditional_t<false __VA_OPT__( | true), types::variadic_t, void>> \
     {                                     \
-    using base = trampoline_ptr_base<RT(A...), types::convention##_t, void>; \
+    using base = trampoline_ptr_base<RT(A...), types::convention##_t, std::conditional_t<false __VA_OPT__( | true), types::variadic_t, void>>; \
     public: \
         trampoline_ptr() : base() {}; \
         trampoline_ptr(std::size_t pointer) : base(reinterpret_cast<base::pointer_type>(pointer)) {} \
@@ -22,23 +22,8 @@ namespace moar
         explicit trampoline_ptr(nullptr_t pointer) : trampoline_ptr(reinterpret_cast<base::pointer_type>(pointer)) {} \
         explicit trampoline_ptr(void* pointer) : base(reinterpret_cast<base::pointer_type>(pointer)) {} \
     };                                    \
-    template<typename RT, typename ...A> requires moar::is_calling_convention_active_v<RT(A...), types::convention##_t, void> \
-    trampoline_ptr(RT convention##_m(A...)) -> trampoline_ptr<RT convention##_m (A...)>;
-
-#define trampoline_ptr_va_impl(convention) \
-    template<typename RT, typename ...A> requires moar::is_calling_convention_active_v<RT(A...), types::convention##_t, types::variadic_t> \
-    class trampoline_ptr<RT convention##_m(A...,...)> final : public trampoline_ptr_base<RT(A...), types::convention##_t, types::variadic_t> \
-    {                                     \
-    using base = trampoline_ptr_base<RT(A...), types::convention##_t, types::variadic_t>; \
-    public: \
-        trampoline_ptr() : base() {}; \
-        trampoline_ptr(std::size_t pointer) : base(reinterpret_cast<base::pointer_type>(pointer)) {} \
-        explicit trampoline_ptr(base::pointer_type pointer) : base(pointer) {}; \
-        explicit trampoline_ptr(nullptr_t pointer) : trampoline_ptr(reinterpret_cast<base::pointer_type>(pointer)) {} \
-        explicit trampoline_ptr(void* pointer) : base(reinterpret_cast<base::pointer_type>(pointer)) {} \
-    };                                    \
-    template<typename RT, typename ...A> requires moar::is_calling_convention_active_v<RT(A...), types::convention##_t, types::variadic_t> \
-    trampoline_ptr(RT convention##_m(A...,...)) -> trampoline_ptr<RT convention##_m (A...,...)>;
+    template<typename RT, typename ...A> requires moar::is_calling_convention_active_v<RT(A...), types::convention##_t, std::conditional_t<false __VA_OPT__( | true), types::variadic_t, void>> \
+    trampoline_ptr(RT convention##_m(A... __VA_OPT__(,__VA_ARGS__))) -> trampoline_ptr<RT convention##_m (A...)>;
 
     trampoline_ptr_impl(cdecl);
     trampoline_ptr_impl(stdcall);
@@ -49,12 +34,11 @@ namespace moar
     trampoline_ptr_impl(sysv_abi);
     trampoline_ptr_impl(ms_abi);
 
-    trampoline_ptr_va_impl(cdecl);
-    trampoline_ptr_va_impl(regcall);
-    trampoline_ptr_va_impl(sysv_abi);
-    trampoline_ptr_va_impl(ms_abi);
+    trampoline_ptr_impl(cdecl, ...);
+    trampoline_ptr_impl(regcall, ...);
+    trampoline_ptr_impl(sysv_abi, ...);
+    trampoline_ptr_impl(ms_abi, ...);
 
-#undef trampoline_ptr_va_impl
 #undef trampoline_ptr_impl
 
 }
